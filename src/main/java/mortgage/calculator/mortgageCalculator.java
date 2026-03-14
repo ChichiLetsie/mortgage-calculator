e07873aa817cccf841a9a59ef58963a09036204c
chichiletsie <chichiangeloletsie@gmail.com>
2026-03-14 22:01:06 +0200

    user validation v2

--- src/main/java/mortgage/calculator/mortgageCalculator.java
+++ src/main/java/mortgage/calculator/mortgageCalculator.java
@@ -16,17 +16,41 @@ public class mortgageCalculator {
 
         //INPUTS
         // principal amount
-        System.out.print("Principal: ");
-        Float principal = scanner.nextFloat();
+        Float principal = 0F;
+        while (true){
+            System.out.print("Principal: ");
+            principal = scanner.nextFloat();
+            if (principal >= 1000 && principal <= 1000000)
+                    break;
+            else {
+                System.out.println("Enter a value between 1000 and 1,000,000");
+        }
+        }
 
         //Annual Interest amount
-        System.out.print("Annual Interest Rate: ");
-        Float annualInterestRate = scanner.nextFloat();
-        Float annualMonthlyRate = annualInterestRate / PERCENT /MONTH_IN_YEARS;
+        Float annualInterestRate = 0F;
+        while (true) {
+            System.out.print("Annual Interest Rate: ");
+            annualInterestRate = scanner.nextFloat();
+            if (annualInterestRate > 0F && annualInterestRate <= 30F) {
+                break;
+            } else {
+                System.out.println("Enter a value greater than 0 or equal to 30");
+            }
+        }
+        Float annualMonthlyRate = annualInterestRate / PERCENT / MONTH_IN_YEARS;
 
         // years in months
-        System.out.print("Period(Years): ");
-        byte years = scanner.nextByte() ;
+        byte years = 0;
+        while (true) {
+            System.out.print("Period(Years): ");
+            years = scanner.nextByte();
+            if (years > 0 && years <= 30) {
+                break;
+            } else{
+                System.out.println("Enter a value between 1 and 30");
+            }
+        }
         int numberOfPayments = years * MONTH_IN_YEARS;
 
         //OUTPUT
